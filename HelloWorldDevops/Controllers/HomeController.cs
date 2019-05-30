be7f05b944c675630091884fb0ffb2f0ac547270
Saravana <kumarkpsaravana@gmail.com>
2019-05-30 23:06:17 +0530

    test

--- HelloWorldDevops/Controllers/HomeController.cs
+++ HelloWorldDevops/Controllers/HomeController.cs
@@ -16,6 +16,8 @@ namespace HelloWorldDevops.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+			ViewData["Message"] = "Your application description page.";
+			ViewData["Message"] = "Your application description page.";
 
             return View();
         }
